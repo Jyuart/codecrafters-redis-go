bad844abd7120b8ec4ba7b94ccec1d92b0fac39c
Yurii Palaida <yura.palayda@gmail.com>
2024-02-02 18:54:32 +0000

    pass stage 2

--- app/server.go
+++ app/server.go
@@ -6,15 +6,31 @@ import (
 	"os"
 )
 
+const PingMessage = "*1\r\n$4\r\nping\r\n"
+const PongResponse = "+PONG\r\n"
+
 func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
+
+	buff := make([]byte, 1024)
+	_, err = conn.Read(buff)
+	if err != nil {
+		fmt.Println("There was an error reading data", err.Error())
+		os.Exit(1)
+	}
+
+	_, err = conn.Write([]byte(PongResponse))
+	if err != nil {
+		fmt.Println("There was an error writing data", err.Error())
 		os.Exit(1)
 	}
 }
